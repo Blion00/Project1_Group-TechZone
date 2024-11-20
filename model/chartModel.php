774c3f7b755407490a4fa480de36d3a3a56ffa59
Gia Bảo <92194463+Blion00@users.noreply.github.com>
2024-11-20 17:21:14 +0700

    first commit

---
+++ model/chartModel.php
@@ -0,0 +1,13 @@
+<!-- <!-- <?php
+// models/ChartModel.php
+class ChartModel {
+    public function getChartData() {
+        return [
+            ['Category', 'Quantity'],
+            ['Red Wine', 45],
+            ['White Wine', 26],
+            ['Rose Wine', 29]
+        ];
+    }
+} 
+?> -->
